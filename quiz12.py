57893b239aa2f75d7511cdef0f78a74c9d7ea4ff
gitae1234 <163506568+gitae1234@users.noreply.github.com>
2024-05-24 16:29:27 +0900

    Add files via upload

--- quiz12.py
+++ quiz12.py
@@ -1,6 +1,22 @@
-class Dog:
-    def __init__(self, name, breed) :
-    self.name = name
-    self.breed = breed
+class Carpbread:
+    def __init__(self, name, price2):
+        self.contents = name
+        self.price = price2
+        self.total = 0
+
+    def sell(self):
+
+        print(self.contents + "붕어빵이 판매 되었습니다.")
+
+cream_carpbread = Carpbread("슈크림", 1000)
+redbean_carpbread = Carpbread("팥", 1000)
+
+#print(cream_carpbread.contents, cream_carpbread.price)
+#print(redbean_carpbread.contents, redbean_carpbread.price)
+
+cream_carpbread.sell()
+redbean_carpbread.sell()
+
+
 
 
